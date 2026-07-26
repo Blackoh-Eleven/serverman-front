9c78cd31a5eca55b6f1d369d85bbd19a3062ed2e
samuel Emmanuel <eminiheminez@gmail.com>
2026-07-26 16:16:48 +0100

    added loading animation

--- servermanfrontend/index.js
+++ servermanfrontend/index.js
@@ -71,7 +71,9 @@ document.getElementById("signupbtn").addEventListener('click' ,function(){
 
 
 
-document.getElementById("signinbtn").addEventListener("click", function () {
+document.getElementById("signinbtn").addEventListener("click", async function () {
+        document.querySelector(".loading").style.display = "flex";
+    document.querySelector(".login-container").style.display = "none";
     let userName = document.getElementById("username");
     let userNameValue = userName.value;
     userName.value = "";
@@ -90,28 +92,41 @@ document.getElementById("signinbtn").addEventListener("click", function () {
         password: passWordValue
     };
 
-    fetch("https://serverman-rh0k.onrender.com/signin", {
+    try{
+    const res = await fetch("https://serverman-rh0k.onrender.com/signin", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(userInfo)
     })
-    .then(res => res.json())
-    .then(data => {
+
+    const data = await res.json()
+    // .then(res => res.json())
+    // .then(data => {
         console.log(data);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
         if (data.token) {
             console.log('he dey',data.token)
            localStorage.setItem("token", data.token);
            const tok = localStorage.getItem("token")
            console.log(tok)
+           document.querySelector(".loading").style.display = "none"
             alert("Login successful!")
             window.location.href = "profile.html";
         } else {
+            document.querySelector(".loading").style.display = "none";
+            document.querySelector(".login-container").style.display = "block";
             alert(data.message);
         }
-    })
-    .catch(err => console.error(err));
+    }catch(err){
+            console.error(err)
+            document.querySelector(".loading").style.display = "none";
+        document.querySelector(".login-container").style.display = "block";
+
+        alert("Unable to connect to the server.");
+        }
+    // })
+    // .catch(err => console.error(err));
 });
 
 
