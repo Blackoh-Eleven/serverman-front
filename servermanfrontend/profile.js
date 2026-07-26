3aab2d5034f16ef23702741ef39c35a7507e11f5
samuel Emmanuel <eminiheminez@gmail.com>
2026-07-26 15:03:04 +0100

    check to see of test is possible

--- servermanfrontend/profile.js
+++ servermanfrontend/profile.js
@@ -1,14 +1,58 @@
-const token = localStorage.getItem("token");
+// const token = localStorage.getItem("token");
+// const loading = document.querySelector(".loading");
+// const content = document.querySelector(".content");
 
-fetch("https://serverman-rh0k.onrender.com/profile", {
-    headers: {
-        Authorization: `Bearer ${token}`
+
+// loading.style.display ="flex"
+
+
+
+
+
+// fetch("https://serverman-rh0k.onrender.com/profile", {
+//     headers: {
+//         Authorization: `Bearer ${token}`
+//     }
+// })
+// .then(res => res.json())
+// .then(profile => {
+//     document.getElementById("boldusername").textContent = profile.profileName;
+//     document.getElementById("emailbox").textContent = profile.email;
+// });
+
+
+const token = localStorage.getItem("token");
+const loading = document.querySelector(".loading");
+const content = document.querySelector(".content");
+
+loading.style.display = "flex";
+content.style.display = "none";
+
+async function loadProfile() {
+    try {
+        const res = await fetch("https://serverman-rh0k.onrender.com/profile", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+
+        const text = await res.text();
+console.log(text);
+
+        // const profile = await res.json();
+
+        // document.getElementById("boldusername").textContent = profile.profileName;
+        // document.getElementById("emailbox").textContent = profile.email;
+
+        // loading.style.display = "none";
+        // content.style.display = "block";
+    } catch (err) {
+        console.error(err);
+        loading.style.display = "none";
     }
-})
-.then(res => res.json())
-.then(profile => {
-    document.getElementById("boldusername").textContent = profile.profileName;
-    document.getElementById("emailbox").textContent = profile.email;
-});
+}
+
+loadProfile();
 
 
