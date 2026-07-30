2a087d3b5279c23ebbbd1eb66ff86fe18f5a689f
samuel Emmanuel <eminiheminez@gmail.com>
2026-07-30 17:54:19 +0100

    added features on profile page

--- servermanfrontend/profile.js
+++ servermanfrontend/profile.js
@@ -12,3 +12,31 @@ fetch("https://serverman-rh0k.onrender.com/profile", {
 });
 
 
+
+
+const sidebar = document.getElementById("sidebar");
+
+let sidecomputed = window.getComputedStyle(sidebar)
+console.log(sidecomputed.display)
+
+document.getElementById("cancelbtn").addEventListener("click", function () {
+    if (sidecomputed.display === "block") {
+        sidebar.style.display = "none";
+        console.log(sidebar.style.display)
+        
+    }
+});
+
+
+document.getElementById("sidebaropen").addEventListener("click", function () {
+    if (sidecomputed.display === "none") {
+        sidebar.style.display = "block";
+        console.log(sidebar.style.display)
+        
+    }
+});
+
+
+
+
+
